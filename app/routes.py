47553d123843a2dcbf045cb8927fdc87ec27d46f
KL-VEDA <veda@kluniversity.in>
2025-09-08 17:51:38 +0530

    Initial Commit

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, jsonify
+
+main = Blueprint('main', __name__)
+
+@main.route("/")
+def index():
+    return jsonify({"message": "Welcome to the Flask API!"})
+
+@main.route("/server-status")
+def server_status():
+    return jsonify({"status": True, "message": "Server is running!"})
